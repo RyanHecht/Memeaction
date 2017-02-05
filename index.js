5c263878a22d21906a5923d6b934119611555ace
RyanHecht <ryanhecht.tse@gmail.com>
2017-02-04 19:07:47 -0500

    Work on sending image from client to server

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express');
+var path    = require("path");
+
 var app = express();
 
 app.get('/', function(req, res, next) {
-  res.send("Hello World!");
-})
+  res.send("Nothing to see here...<i>yet!</i>");
+});
 
+app.get('/image', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/takePicture.html'));
+});
+
+app.post('/uploadImage', function(req, res, next) {
+   console.log("got it");
+   console.log(req.data);
+});
+app.get('/js/:file', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/js/captureImage.js'));
+});
 
 app.listen('8080', function() {
   console.log("Server running!")
-})
+});
