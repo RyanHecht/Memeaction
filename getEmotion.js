b37487b986b05decafacbad704c7fd07de146f3f
RyanHecht <ryanhecht.tse@gmail.com>
2017-02-05 01:41:19 -0500

    WHAT IS A PROMISE?

--- getEmotion.js
+++ getEmotion.js
@@ -1,22 +1,21 @@
-var request = require('request');
+var request = require('request-promise');
 module.exports =
 function(imgID) {
-
-  var object = { "url": "http://memeaction.azurewebsites.net/img/" + imgID + ".png" }
-  var resBody = {};
-  request({
-    url: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+  var options = {
     method: 'POST',
+    uri: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+    body: { "url": "http://memeaction.azurewebsites.net/img/" + imgID + ".png" },
+    json: true,
     headers: {
       'Content-Type':     'application/json',
       'Ocp-Apim-Subscription-Key': '9fa170b54cd845d787ca0bee2dfd9daf'
-    },
-    json: object
-}, function(error, response, body){
-    if(error) {
-    } else {
-        resBody = body
     }
-});
-return resBody
+  };
+  return request(options)
+    .then(function (body) {
+      return body;
+    })
+    .catch(function (err) {
+      console.log('err');
+    })
 };
