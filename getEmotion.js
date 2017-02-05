85c905bc5874f3c33f160adc5c3ec72d807e4fe4
RyanHecht <ryanhecht.tse@gmail.com>
2017-02-05 04:26:02 -0500

    fix image link

--- getEmotion.js
+++ getEmotion.js
@@ -2,10 +2,11 @@ var request = require('request-promise');
 
 module.exports =
 function(imgID) {
+  //console.log()
   var options = {
     method: 'POST',
     uri: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
-    body: { "url": "http://memeaction.azurewebsites.net/img/" + imgID + ".png" },
+    body: { "url": "https://memeaction.azurewebsites.net/img/" + imgID + ".png" },
     json: true,
     headers: {
       'Content-Type':     'application/json',
